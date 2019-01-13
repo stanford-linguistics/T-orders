fddacfa833103efefed687b2c0b25c6594ce2d2a
Juan Solis <jsolis@graycoltdevelopers.com>
2019-01-12 20:25:36 -0700

    Add uploads route

--- api/app.py
+++ api/app.py
@@ -1,9 +1,57 @@
-from flask import Flask
-from flask import url_for
+import os
+from flask import Flask, flash, request, redirect, url_for
+from flask_api import status
+from werkzeug.utils import secure_filename
 from worker import celery
 import celery.states as states
+from celery.utils import uuid
+
+RESULTS_FOLDER = '/results'
+ALLOWED_EXTENSIONS = set(['xls'])
 
 app = Flask(__name__)
+app.config['RESULTS_FOLDER'] = RESULTS_FOLDER
+
+def no_file_was_uploaded(files):
+    return 'file' not in files
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def is_invalid_file(file):
+    filename = file.filename
+    return filename == '' or not allowed_file(filename)
+
+def create_input_directory(file_id):
+    directory = RESULTS_FOLDER + '/' + file_id + '/input'
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+def save_file_to_disk(file, file_id):
+    create_input_directory(file_id)
+    filename = secure_filename(file.filename)
+    input_file_path = os.path.join(app.config['RESULTS_FOLDER'], file_id, 'input', filename)
+    file.save(input_file_path)
+
+
+@app.route('/uploads', methods=['POST'])
+def upload_file():
+    if no_file_was_uploaded(request.files):
+        return 'No file received', status.HTTP_400_BAD_REQUEST
+    
+    elif is_invalid_file(request.files['file']):
+        message = 'Invalid file received. Only file types of ' + ', '.join(ALLOWED_EXTENSIONS) + ' are allowed.'
+        return message, status.HTTP_400_BAD_REQUEST
+
+    else:
+        file = request.files['file']
+        file_id = uuid()
+        save_file_to_disk(file, file_id)
+        task = celery.send_task('tasks.compute_t_orders', args=[], kwargs={}, task_id=file_id)
+ 
+        response = f"<a href='{url_for('check_task', task_id=task.id, external=True)}'>check status of {task.id}</a>"
+        return response
 
 
 @app.route('/add/<int:param1>/<int:param2>')
--- celery-queue/tasks.py
+++ celery-queue/tasks.py
@@ -2,10 +2,17 @@ import os
 import time
 from celery import Celery
 
+RESULTS_FOLDER = '/results'
+
 CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL', 'redis://localhost:6379'),
 CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND', 'redis://localhost:6379')
 
 celery = Celery('tasks', broker=CELERY_BROKER_URL, backend=CELERY_RESULT_BACKEND)
+
+@celery.task(name='tasks.compute_t_orders',bind=True)
+def compute_t_orders(self):
+    file_id = self.request.id
+    return 'T-Order computed!'
 
 @celery.task(name='tasks.add')
 def add(x, y):
