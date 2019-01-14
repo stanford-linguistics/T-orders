554bb045d2f98e61d49549efd7e0e89ecebe1b72
Juan Solis <jsolis@henselphelps.com>
2019-01-13 21:36:13 -0700

    Add download route

--- api/app.py
+++ api/app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, flash, request, redirect, url_for
+from flask import Flask, flash, request, redirect, url_for, send_from_directory
 from flask_api import status
 from werkzeug.utils import secure_filename
 from worker import celery
@@ -12,54 +12,81 @@ ALLOWED_EXTENSIONS = set(['xls'])
 app = Flask(__name__)
 app.config['RESULTS_FOLDER'] = RESULTS_FOLDER
 
+
 def no_file_was_uploaded(files):
     return 'file' not in files
+
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+
 def is_invalid_file(file):
     filename = file.filename
     return filename == '' or not allowed_file(filename)
+
 
 def create_input_directory(file_id):
     directory = RESULTS_FOLDER + '/' + file_id + '/input'
     if not os.path.exists(directory):
         os.makedirs(directory)
 
+
 def save_file_to_disk(file, file_id):
     create_input_directory(file_id)
     filename = secure_filename(file.filename)
-    input_file_path = os.path.join(app.config['RESULTS_FOLDER'], file_id, 'input', filename)
+    input_file_path = os.path.join(
+        app.config['RESULTS_FOLDER'], file_id, 'input', filename)
     file.save(input_file_path)
     return input_file_path, filename
+
 
 @app.route('/uploads', methods=['POST'])
 def upload_file():
     if no_file_was_uploaded(request.files):
         return 'No file received', status.HTTP_400_BAD_REQUEST
-    
+
     elif is_invalid_file(request.files['file']):
-        message = 'Invalid file received. Only file types of ' + ', '.join(ALLOWED_EXTENSIONS) + ' are allowed.'
+        message = 'Invalid file received. Only file types of ' + \
+            ', '.join(ALLOWED_EXTENSIONS) + ' are allowed.'
         return message, status.HTTP_400_BAD_REQUEST
 
     else:
         file = request.files['file']
         file_id = uuid()
         input_file_path, filename = save_file_to_disk(file, file_id)
-        task = celery.send_task('tasks.compute_t_orders', args=[input_file_path, filename], kwargs={}, task_id=file_id)
- 
+        task = celery.send_task('tasks.compute_t_orders', args=[
+                                input_file_path, filename], kwargs={}, task_id=file_id)
+
         response = f"<a href='{url_for('check_task', task_id=task.id, external=True)}'>check status of {task.id}</a>"
         return response
 
-@app.route('/add/<int:param1>/<int:param2>')
-def add(param1: int, param2: int) -> str:
-    task = celery.send_task('tasks.add', args=[param1, param2], kwargs={})
-    response = f"<a href='{url_for('check_task', task_id=task.id, external=True)}'>check status of {task.id} </a>"
-    return response
 
-@app.route('/check/<string:task_id>')
+def get_zip_filename(directory):
+    zip_filename = ''
+    for file in os.listdir(directory):
+        if file.endswith(".zip"):
+            zip_filename = file
+            break
+    return zip_filename
+
+
+@app.route('/results/<string:task_id>/$value', methods=['GET'])
+def download_file(task_id):
+    directory = os.path.join(app.config['RESULTS_FOLDER'], task_id)
+    if os.path.exists(directory):
+        zip_filename = get_zip_filename(directory)
+        if zip_filename != '':
+            return send_from_directory(directory,
+                                zip_filename, as_attachment=True)
+        else:
+            return 'No file belonging to id: ' + task_id + ' was found.', HTTP_404_NOT_FOUND
+    else:
+        return 'No file belonging to id: ' + task_id + ' was found.', HTTP_404_NOT_FOUND
+
+
+@app.route('/results/<string:task_id>')
 def check_task(task_id: str) -> str:
     res = celery.AsyncResult(task_id)
     if res.state == states.PENDING:
--- celery-queue/tasks.py
+++ celery-queue/tasks.py
@@ -5,29 +5,42 @@ import results_helper
 
 RESULTS_FOLDER = '/results'
 
-CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL', 'redis://localhost:6379'),
-CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND', 'redis://localhost:6379')
+CELERY_BROKER_URL = os.environ.get(
+    'CELERY_BROKER_URL', 'redis://localhost:6379'),
+CELERY_RESULT_BACKEND = os.environ.get(
+    'CELERY_RESULT_BACKEND', 'redis://localhost:6379')
 
-celery = Celery('tasks', broker=CELERY_BROKER_URL, backend=CELERY_RESULT_BACKEND)
+celery = Celery('tasks', broker=CELERY_BROKER_URL,
+                backend=CELERY_RESULT_BACKEND)
+
 
 def get_output_path(folder_id):
     return os.path.join(RESULTS_FOLDER, folder_id, 'output')
 
+
 def call_t_order(input_file_path, output_path):
-    os.system('python t_orders.py ' +  input_file_path + ' --output ' + output_path)
+    os.system('python t_orders.py ' + input_file_path +
+              ' --output ' + output_path)
+
 
 def get_task_results_path(folder_id):
     return os.path.join(RESULTS_FOLDER, folder_id)
+
 
 def zip_results(input_filename, folder_id):
     directory_to_zip = get_task_results_path(folder_id)
     zip_name = os.path.splitext(input_filename)[0] + '.zip'
     results_helper.zip_all(directory_to_zip, zip_name)
 
+
 def clean_results(folder_id):
     directory_to_clean = get_task_results_path(folder_id)
     results_helper.clean_directory(os.path.join(directory_to_clean, 'input'))
     results_helper.clean_directory(os.path.join(directory_to_clean, 'output'))
+
+
+def get_download_url(folder_id):
+    return '/results/' + folder_id + '/$value?external=True'
 
 @celery.task(name='tasks.compute_t_orders', bind=True)
 def compute_t_orders(self, input_file_path, input_filename):
@@ -35,7 +48,8 @@ def compute_t_orders(self, input_file_path, input_filename):
     call_t_order(input_file_path, get_output_path(folder_id))
     zip_results(input_filename, folder_id)
     clean_results(folder_id)
-    return 'T-order computed!'
+    return get_download_url(folder_id)
+
 
 @celery.task(name='tasks.add')
 def add(x, y):
