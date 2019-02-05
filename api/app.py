e3da4925e8024a156b30212573446d9ddf5e930e
Juan Solis <jsolis@henselphelps.com>
2019-02-04 20:25:07 -0700

    Enable CORS

--- api/app.py
+++ api/app.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask, Blueprint
+from flask_cors import CORS
 from routes import *
 
 # Constants
@@ -9,6 +10,7 @@ OUTPUT_FILE_EXTENSION = '.zip'
 ALLOWED_EXTENSIONS = set(['xls'])
 
 app = Flask(__name__)
+CORS(app)
 app.config['RESULTS_FOLDER'] = RESULTS_FOLDER
 app.config['INPUT_FILE_EXTENSION'] = INPUT_FILE_EXTENSION
 app.config['OUTPUT_FILE_EXTENSION'] = OUTPUT_FILE_EXTENSION
